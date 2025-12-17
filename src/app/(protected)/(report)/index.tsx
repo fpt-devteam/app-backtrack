bf7673b892ba3d990a1c7030e5e02e5d07c7ceac
PhiLong1st <philongnguyen1704@gmail.com>
2025-12-18 00:44:07 +0700

    feat: add upload image to firebase storage

--- src/app/(protected)/(report)/index.tsx
+++ src/app/(protected)/(report)/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Button, Text, View } from 'react-native';
 
 const ReportScreen = () => {
+  const router = useRouter();
+
   return (
     <View>
       <Text>This is ReportScreen will show list of reports</Text>
+      <Button title="Create Report" onPress={() => router.push('/(protected)/(report)/create')} />
     </View>
   )
 }
---
+++ src/types/firebase.type.ts
@@ -0,0 +1,21 @@
+import { ImagePickerAsset } from "expo-image-picker";
+
+export type ImageAsset = ImagePickerAsset & {
+  type: 'image';
+};
+
+export type ImageUploadRequest = {
+  filename: string;
+  firebaseUid: string;
+  uri: string;
+  path: string;
+  blob: Blob;
+};
+
+export type ImageUploadResponse = {
+  downloadURL: string;
+};
+
+export type ReportImageUploadRequest = ImageUploadRequest & {
+  postId: string;
+};
