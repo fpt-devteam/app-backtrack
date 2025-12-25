79dabf35edb62b4e7541d72e4a2ae43e5fa2c702
PhiLong1st <philongnguyen1704@gmail.com>
2025-12-25 15:05:56 +0700

    feat: Replace ImagePickerSection with ImageField and update image handling logic

--- src/features/item/components/ItemCreateForm/ItemCreateForm.tsx
+++ src/features/item/components/ItemCreateForm/ItemCreateForm.tsx
@@ -1,9 +1,9 @@
-import ImagePickerSection from "@/src/shared/components/ImagePickerSection/ImagePickerSection";
+import ImageField from "@/src/shared/components/ImageField/ImageField";
 import { useUploadImage } from "@/src/shared/hooks/useUploadImage";
-import { ImageAsset } from "@/src/shared/types/firebase.type";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { ImagePickerAsset } from "expo-image-picker";
 import { router } from "expo-router";
-import React, { useState } from 'react';
+import React from 'react';
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import * as yup from "yup";
@@ -16,40 +16,66 @@ const qrSchema = yup
   .object({
     name: yup.string().required("Name is required"),
     description: yup.string().required("Description is required"),
+    images: yup
+      .array()
+      .of(
+        yup
+          .mixed<ImagePickerAsset>().defined()
+          .test("has-uri", "Invalid image (missing uri).", (asset) => {
+            return !!asset && typeof asset === "object" && !!asset.uri;
+          })
+          .test("mime", "Only JPG/PNG/WebP images are allowed.", (asset) => {
+            if (!asset) return true;
+
+            const mime = asset.mimeType;
+            if (!mime) return true;
+
+            const allowedMimes = ["image/jpeg", "image/png", "image/webp"];
+            return allowedMimes.includes(mime);
+          })
+          .test("size", "Image is too large (max 5 MB).", (asset) => {
+            if (!asset) return true;
+
+            const size = asset.fileSize;
+            if (typeof size !== "number") return true;
+
+            const maxImageSize = 5 * 1024 * 1024; // 5 MB
+            return size <= maxImageSize;
+          })
+      )
+      .min(1, "Please select at least 1 image.")
+      .max(5, "You can select up to 5 images.")
+      .required("Images are required."),
   })
   .required();
 
 type ItemSchema = yup.InferType<typeof qrSchema>;
 
 const ItemLinkForm = () => {
-  const [images, setImages] = useState<ImageAsset[]>([]);
   const { uploadImages } = useUploadImage();
   const { linkItemToQr } = useLinkQrItem();
 
-  const pickImages = (pickedImages: ImageAsset[]) => {
-    setImages(pickedImages);
-  };
-
-  const handleUploadImages = async () => {
+  const handleUploadImages = async (images: ImagePickerAsset[]) => {
     const uploadRes = await uploadImages(images);
-    if (!uploadRes) return null;
+    if (!uploadRes) return [];
 
-    const imageUrls = uploadRes.map((res) => res.downloadURL);
+    const imageUrls = uploadRes.map((res: { downloadURL: string }) => res.downloadURL);
     return imageUrls;
-  }
+  };
 
   const { control, handleSubmit, formState: { errors },
   } = useForm<ItemSchema>({
     defaultValues: {
       name: "",
       description: "",
+      images: [] as ImagePickerAsset[],
     },
     resolver: yupResolver(qrSchema),
     mode: "onSubmit",
   });
 
   const onSubmit: SubmitHandler<ItemSchema> = async (data: ItemSchema) => {
-    const imageUrls = await handleUploadImages();
+    const imageUrls = await handleUploadImages(data.images);
     if (!imageUrls) {
       Alert.alert("Error", "Failed to upload images.");
       return;
@@ -80,7 +106,17 @@ const ItemLinkForm = () => {
         <Text style={styles.title}>Create QR Code for Item</Text>
 
         {/* Image Picker */}
-        <ImagePickerSection images={images} pickImages={pickImages} />
+        <View style={styles.fieldContainer}>
+          <Text style={styles.label}>Photos of the Item *</Text>
+          <Controller
+            control={control}
+            name="images"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <ImageField value={value} onChange={onChange} />
+            )}
+          />
+          {errors.images && (<Text style={styles.errorText}>{errors.images.message}</Text>)}
+        </View>
 
         {/* Item Name */}
         <View style={styles.fieldContainer}>
