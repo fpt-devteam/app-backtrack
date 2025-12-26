5139e00dd562b6079a5da6a1d8c352befbdb1a7d
PhiLong1st <philongnguyen1704@gmail.com>
2025-12-26 12:09:43 +0700

    feat: Refactor ProfileScreen component and remove ProfileInformation screen

--- src/app/(protected)/(profile)/index.tsx
+++ src/app/(protected)/(profile)/index.tsx
@@ -1,12 +1,10 @@
-import { auth } from "@/src/lib/firebase";
-import { useAuth } from "@/src/providers/AuthProvider";
-import { ExternalPathString, useRouter } from "expo-router";
+import { useAuth } from "@/src/features/auth/providers/AuthProvider";
+import { auth } from "@/src/shared/lib/firebase";
 import { signOut } from "firebase/auth";
 import React from "react";
 import { Button, Text, View } from "react-native";
 
 export default function ProfileScreen() {
-  const router = useRouter();
   const { clearSession } = useAuth();
 
   const handleLogout = async () => {
@@ -17,9 +15,6 @@ export default function ProfileScreen() {
   return (
     <View style={{ padding: 16, gap: 12 }}>
       <Text>ProfileScreen</Text>
-      <Button title="Profile Information" onPress={() => {
-        router.push(`/(protected)/(profile)/profileInformation` as ExternalPathString);
-      }} />
       <Button title="Logout" onPress={handleLogout} />
     </View>
   );
