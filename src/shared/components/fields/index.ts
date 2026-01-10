8606b5d26fa75229840295a140dddd8456434449
PhiLong1st <philongnguyen1704@gmail.com>
2026-01-10 18:23:16 +0700

    refactor: refactor shared feature, import path by rule, do not use type any

--- src/shared/components/fields/index.ts
+++ src/shared/components/fields/index.ts
@@ -2,6 +2,5 @@ export { default as ConfirmPasswordField } from "./ConfirmPasswordField";
 export { default as DateTimePickerField } from "./DateTimePickerField";
 export { default as EmailField } from "./EmailField";
 export { default as ImageField } from "./ImageField";
-export { default as LocationField } from "./LocationField";
 export { default as PasswordField } from "./PasswordField";
 
