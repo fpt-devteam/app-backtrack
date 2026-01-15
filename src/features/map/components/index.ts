7db10bc64e5f7a758295b7e68fb682dd8c15f2b0
PhiLong1st <philongnguyen1704@gmail.com>
2026-01-16 01:29:49 +0700

    feat: refactor map and search components, integrate autocomplete functionality

--- src/features/map/components/index.ts
+++ src/features/map/components/index.ts
@@ -1,5 +1,4 @@
 export { default as ItemPlaceMarker } from './ItemPlaceMarker';
-export { default as MapSearchBar } from './MapSearchBar';
 export { default as UserPlaceButton } from './UserPlaceButton';
 export { default as UserPlaceMarker } from './UserPlaceMarker';
 
