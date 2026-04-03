d12eaef4dcf8aa0bcfef2a17eb8733664462a52c
PhiLong1st <philongnguyen1704@gmail.com>
2026-04-03 15:08:35 +0700

    refactor: clean up imports and remove redundant comments in post and QR screens

--- src/features/post/types/post.dto.ts
+++ src/features/post/types/post.dto.ts
@@ -86,14 +86,3 @@ export type PostSearchResponse = ApiResponse<PagedResponse<Post>>;
 
 // Post Suggestion
 export type PostsSuggestionResponse = ApiResponse<PostSuggestion[]>;
-
-
-/*
- * Post Search Request and Response
-*/
-export type PostSearchRequest = PostSearchOptions & PagedRequest
-
-export type PostSearchResponse = ApiResponse<PagedResponse<Post>>;
-
-// Post Suggestion
-export type PostsSuggestionResponse = ApiResponse<PostSuggestion[]>;
